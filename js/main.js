f3e61135f01a50982514b795a69df85e81698b46
Hayden Burnell <71723408+hburne11@users.noreply.github.com>
2023-02-15 16:20:01 -0500

    vue router demo

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// import your modules/components here
+import LoginPage from "./modules/LoginPage.js";
+import HomePage from "./modules/HomePage.js";
+import ErrorPage from "./modules/ErrorPage.js";
+
+
+const router = VueRouter.createRouter({
+    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+    history: VueRouter.createWebHashHistory(),
+    routes: [
+            // vue will try to match the following routes and render the appropriate component onto the page
+        {
+            path: '/', // the location bar URL
+            name: 'login', // the name of the route
+            component: LoginPage // the component to render
+        },
+
+        {
+            path: '/home',
+            name: 'home',
+            component: HomePage
+        },
+
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'error',
+            component: ErrorPage
+        }
+    ] // short for `routes: routes`
+  })
+  
+  // 5. Create and mount the root instance.
+  const app = Vue.createApp({
+    methods: {
+        tryRouterPush() {
+            this.$router.push({ name: 'home'});
+        }
+    }
+  })
+  // Make sure to _use_ the router instance to make the
+  // whole app router-aware.
+  app.use(router)
+  
+  app.mount('#app')
--- js/modules/ErrorPage.js
+++ js/modules/ErrorPage.js
@@ -1,3 +1,12 @@
 export default {
-    template: `<h1>That page doesn't exist!</h1>`
+    template: `<h1>That page doesn't exist!</h1>`,
+
+    created() {
+        console.log('hit the error route');
+
+        //redirect back to safe route (home page)
+        setTimeout(()=> {
+            this.$router.push({ name: 'home'});
+        }, 2500); // wait 2.5 secs and then execute the function
+    }
 }
